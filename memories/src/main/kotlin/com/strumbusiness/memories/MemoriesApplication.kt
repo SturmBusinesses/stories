3a73a7e2bb048bc1ec6843376a1e10b0c96c783a
Adam Wilson <adamwilson@usernamesuggestion-MBP2023-2.local>
2023-08-28 17:58:04 -0700

    28 Aug 2023 Added Kotlin spring boot project for backend.

---
+++ memories/src/main/kotlin/com/strumbusiness/memories/MemoriesApplication.kt
@@ -0,0 +1,11 @@
+package com.strumbusiness.memories
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MemoriesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MemoriesApplication>(*args)
+}
---
+++ memories/src/test/kotlin/com/strumbusiness/memories/MemoriesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.strumbusiness.memories
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemoriesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
